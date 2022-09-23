0a43a4fa59135d6537d72fc11f6fccbdef8afd42
dania <daniadeyar@gmail.com>
2022-09-23 19:10:20 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  return console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  return console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+  if (string.toLowerCase() == string.toLowerCase()) {
+    return "I can't hear you!";
+  } else if (string.toUpperCase() == string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if (string === "Let's have dinner together!") {
+    return "I would love to!";
+  } else {
+    return "you have error";
+  }
+}
